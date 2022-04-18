442632494f5b8785bd3ff471b20fb4847465aa79
masonjar <mason.whitaker79@gmail.com>
2022-04-18 08:05:45 -0700

    Committing Lab12 Card 1 work

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -2,13 +2,16 @@ import React from "react";
 import axios from "axios";
 import Carousel from "react-bootstrap/Carousel";
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 import bookImg from "./book.jpeg";
+import NewBookForm from "./newBookForm.js";
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: [],
+      modalSwitch: false,
     };
   }
 
@@ -26,12 +29,26 @@ class BestBooks extends React.Component {
     }
   };
 
+  handleClose = () => {this.setState({modalSwitch: false})}
+  createBook = async (book) => {
+    const config = {
+      method: "post",
+      baseURL: process.env.REACT_APP_SERVER,
+      url: "/books/",
+      data: book,
+    };
+    let bookresults = await axios(config);
+    const updateBooks = [...this.state.books, bookresults.data];
+    this.setState({books: updateBooks})
+  }
   render() {
-    /* TODO: render all the books in a Carousel */
-
     return (
-      <>
+      <div className="center">
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <Button onClick={() => this.setState({modalSwitch: true})}>Add a Book!</Button>
+        {this.state.modalSwitch && (
+          <NewBookForm modalSwitch={this.state.modalSwitch} handleClose={this.handleClose} createBook={this.createBook} />)
+          }
 
         {this.state.books.length ? (
         <Container>
@@ -40,12 +57,10 @@ class BestBooks extends React.Component {
               <Carousel.Item key={book._id}>
                 <img className="d-block w-100" src={bookImg} alt={book.title} />
                 <Carousel.Caption>
-                  <h3>{book.title}</h3>
-                  <h4>By {book.author}.</h4>
-                  <p>
-                    {book.series} Book {book.book}
-                  </p>
-                  <p>{book.desc}</p>
+                  {book.title ? (<h3>{book.title}</h3>) : (<h3>Unknown title</h3>)}
+                  {book.author ? (<h4>By {book.author}.</h4>) : (<h4>Unknown Author</h4>)}
+                  {book.series && book.book ? (<p>{book.series} Book {book.book}.</p>) : (<p>Single Book</p>)}
+                  {book.desc ? (<p>{book.desc}</p>) : (<p>No Description Available</p>)}
                 </Carousel.Caption>
               </Carousel.Item>
             ))}
@@ -54,7 +69,7 @@ class BestBooks extends React.Component {
         ) : (
           <h3>No Books Found :(</h3>
         )}
-      </>
+      </div>
     );
   }
 }
---
+++ src/newBookForm.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+
+class NewBookForm extends React.Component {
+  newBook = (e) => {
+    e.preventDefault()
+    const newBook = {
+      title: e.target.bookName.value,
+      desc: e.target.desc.value,
+      author: e.target.author.value,
+      series: e.target.series.value,
+      book: e.target.bookNum.value,
+      status: e.target.status.value,
+    }
+    this.props.createBook(newBook);
+    this.props.handleClose();
+  }
+  render() {
+    return (
+      <Modal show={this.props.modalSwitch} onHide={this.props.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create a New Book</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={this.newBook}>
+          <Form.Group controlId="bookName">
+            <Form.Label>Book Name</Form.Label>
+            <Form.Control type="text" placeholder="Book Name" />
+          </Form.Group>
+          <Form.Group controlId="desc">
+            <Form.Label>Book Description</Form.Label>
+            <Form.Control type="text" placeholder="Book Description" />
+          </Form.Group>
+          <Form.Group controlId="author">
+            <Form.Label>Book Author</Form.Label>
+            <Form.Control type="text" placeholder="Book Author" />
+          </Form.Group>
+          <Form.Group controlId="series">
+            <Form.Label>Book Series</Form.Label>
+            <Form.Control type="text" placeholder="Book Series" />
+          </Form.Group>
+          <Form.Group controlId="bookNum">
+            <Form.Label>Book Number</Form.Label>
+            <Form.Control type="text" placeholder="Book Number" />
+          </Form.Group>
+          <Form.Group controlId="status">
+            <Form.Label>Book Status</Form.Label>
+            <Form.Control as="select">
+              <option value="FAVROITE">FAVORITE</option>
+              <option value="RECOMENDED">RECOMENDED</option>
+            </Form.Control>
+          </Form.Group>
+          <Button type="submit">Add Book</Button>
+        </Form>
+        <Modal.Footer>
+          <Button>Cancel</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+export default NewBookForm;
